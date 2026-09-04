a23b47a0afb0403ed687d1cdfe8ce20260c5e891
Sanjaijv <sanjaijv.cse2024@citchennai.net>
2026-09-04 21:23:13 +0530

    fix: load API_KEY from environment instead of hardcoding it (resolves gitleaks finding)

--- vulnerable_example.py
+++ vulnerable_example.py
@@ -1,7 +1,6 @@
 import os
 
-# Hardcoded credential (gitleaks should flag this as a generic secret)
-API_KEY = "gK9dP2xQmR7vT4nL8wY1cE6jH3sA0zV5bF9uI2oC7yN4kM8xR1nQ8"
+API_KEY = os.environ.get("API_KEY", "")
 
 def run_query(user_input):
     query = "SELECT * FROM users WHERE name = '" + user_input + "'"
