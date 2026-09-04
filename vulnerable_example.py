943631f715b3d45083433b9e269318704ba7d833
Sanjaijv <sanjaijv.cse2024@citchennai.net>
2026-09-04 22:50:38 +0530

    fix: use parameterized query instead of string concatenation

--- vulnerable_example.py
+++ vulnerable_example.py
@@ -1,7 +1,9 @@
 import os
+import sqlite3
 
 API_KEY = os.environ.get("API_KEY", "")
 
-def run_query(user_input):
-    query = "SELECT * FROM users WHERE name = '" + user_input + "'"
-    os.system(query)
+
+def run_query(connection: sqlite3.Connection, user_input: str) -> list:
+    cursor = connection.execute("SELECT * FROM users WHERE name = ?", (user_input,))
+    return cursor.fetchall()
